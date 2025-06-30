ec77e7929e3dde32155c2fb40c719113686d96f9
Jason Beard <jb71@sanger.ac.uk>
2025-06-30 16:22:32 +0100

    set custom lineage var

--- lineage_calling/run_mykrobe_lineage_calling.py
+++ lineage_calling/run_mykrobe_lineage_calling.py
@@ -9,8 +9,8 @@ def get_sequence(line):
     ID, sequence1, sequence2 = line.strip().split(",")
     return ID, sequence1, sequence2
 
-def run_mykrobe_lineage_call(probe_directory, sequence_manifest,json_directory):
-    check_lineage_file(probe_directory)
+def run_mykrobe_lineage_call(probe_json_directory, sequence_manifest,json_directory):
+    check_lineage_file(probe_json_directory)
 
     with open(sequence_manifest, "r") as manifest:
         next(manifest)  # Skip header line
@@ -19,13 +19,12 @@ def run_mykrobe_lineage_call(probe_directory, sequence_manifest,json_directory):
                 continue  # Skip empty lines
             
             ID, sequence1, sequence2 = get_sequence(line)
-
             sequences = [sequence1]
             if sequence2:
                 sequences.append(sequence2)
 
             args = Namespace(
-                custom_probe_set_path=f"{probe_directory}/probes.fa",
+                custom_probe_set_path=f"{probe_json_directory}/probes.fa",
                 species="custom",
                 report_all_calls=True,
                 tmp=None,
@@ -37,19 +36,19 @@ def run_mykrobe_lineage_call(probe_directory, sequence_manifest,json_directory):
                 skeleton_dir="data/skeletons/",
                 memory="1GB",
                 sample=ID,
-                filters=["MISSING_WT", "LOW_PERCENT_COVERAGE", "LOW_GT_CONF", "LOW_TOTAL_DEPTH"],
-                min_variant_conf=150,
+                filters=[],
+                min_variant_conf=100,
                 min_gene_conf=1,
                 model="kmer_count",
                 min_proportion_expected_depth=0.3,
                 ploidy="diploid",
                 conf_percent_cutoff=100,
-                min_depth=100,
+                min_depth=1,
                 ignore_minor_calls=False,
                 keep_tmp=False,
                 ncbi_names=None,
                 custom_variant_to_resistance_json=None,
-                custom_lineage_json=None,
+                custom_lineage_json=f"{probe_json_directory}/lineage.json",
                 expected_error_rate=0.05,
                 guess_sequence_method=False,
                 output_format="json",
